7bdea6c18275e88ace9e8e19fe449c7a356daecb
Amogelang Mphela <G22A7399@ict.ru.ac.za>
2024-05-22 21:53:13 +0200

    changed the Java application to only output user-selected fields from that XML file

--- theapp.java
+++ theapp.java
@@ -5,10 +5,25 @@ import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 import java.io.File;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 public class theapp{
 
     public static void main(String[] args) {
+        // Define the available fields
+        Set<String> availableFields = new HashSet<>();
+        availableFields.add("name");
+        availableFields.add("postalZip");
+        availableFields.add("region");
+        availableFields.add("country");
+        availableFields.add("address");
+        availableFields.add("list");
+        
+        // Get user-selected fields
+        Set<String> selectedFields = getUserSelectedFields(availableFields);
+
         try {
             File inputFile = new File("data.xml"); // specify the path to your XML file
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
@@ -24,31 +39,69 @@ public class theapp{
                 
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element eElement = (Element) nNode;
-                    
-                    String name = eElement.getElementsByTagName("name").item(0).getTextContent();
-                    String postalZip = eElement.getElementsByTagName("postalZip").item(0).getTextContent();
-                    String region = eElement.getElementsByTagName("region").item(0).getTextContent();
-                    String country = eElement.getElementsByTagName("country").item(0).getTextContent();
-                    String address = eElement.getElementsByTagName("address").item(0).getTextContent();
-                    String listString = eElement.getElementsByTagName("list").item(0).getTextContent();
-                    
-                    // If the list element is empty, handle it accordingly
-                    String[] list = listString.isEmpty() ? new String[0] : listString.split(", ");
-                    
-                    System.out.println("Name : " + name);
-                    System.out.println("Postal/Zip : " + postalZip);
-                    System.out.println("Region : " + region);
-                    System.out.println("Country : " + country);
-                    System.out.println("Address : " + address);
-                    System.out.print("List : ");
-                    for (String item : list) {
-                        System.out.print(item + " ");
+
+                    if (selectedFields.contains("name")) {
+                        String name = eElement.getElementsByTagName("name").item(0).getTextContent();
+                        System.out.println("Name: " + name);
                     }
-                    System.out.println("\n");
+
+                    if (selectedFields.contains("postalZip")) {
+                        String postalZip = eElement.getElementsByTagName("postalZip").item(0).getTextContent();
+                        System.out.println("Postal/Zip: " + postalZip);
+                    }
+
+                    if (selectedFields.contains("region")) {
+                        String region = eElement.getElementsByTagName("region").item(0).getTextContent();
+                        System.out.println("Region: " + region);
+                    }
+
+                    if (selectedFields.contains("country")) {
+                        String country = eElement.getElementsByTagName("country").item(0).getTextContent();
+                        System.out.println("Country: " + country);
+                    }
+
+                    if (selectedFields.contains("address")) {
+                        String address = eElement.getElementsByTagName("address").item(0).getTextContent();
+                        System.out.println("Address: " + address);
+                    }
+
+                    if (selectedFields.contains("list")) {
+                        String listString = eElement.getElementsByTagName("list").item(0).getTextContent();
+                        String[] list = listString.isEmpty() ? new String[0] : listString.split(", ");
+                        System.out.print("List: ");
+                        for (String item : list) {
+                            System.out.print(item + " ");
+                        }
+                        System.out.println();
+                    }
+
+                    System.out.println();
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static Set<String> getUserSelectedFields(Set<String> availableFields) {
+        Set<String> selectedFields = new HashSet<>();
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Available fields: " + availableFields);
+        System.out.println("Enter the fields you want to display, separated by commas:");
+        String input = scanner.nextLine();
+        scanner.close();
+        
+        String[] fields = input.split(",");
+        for (String field : fields) {
+            field = field.trim();
+            if (availableFields.contains(field)) {
+                selectedFields.add(field);
+            } else {
+                System.out.println("Invalid field: " + field);
+            }
+        }
+        
+        return selectedFields;
     }
 }
